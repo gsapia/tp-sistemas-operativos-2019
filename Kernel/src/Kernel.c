e66953384170c636d7292ff8e3b7e4e3ccce19ff
Guillermo Sapia <guillermosapia.lsp@hotmail.com.ar>
2019-04-12 18:19:24 -0300

    Agrego algo de codigo y funciones para sockets

--- Kernel/src/Kernel.c
+++ Kernel/src/Kernel.c
@@ -2,5 +2,27 @@
 
 int main(void) {
 	puts("Soy Kernel");
+
+	int socketPedido;
+		struct sockaddr_in direccionServidor;
+		direccionServidor.sin_family = AF_INET;
+		direccionServidor.sin_addr.s_addr = INADDR_ANY;
+		direccionServidor.sin_port = htons(6060); //Puerto que tiene que escuchar
+
+
+	    socketPedido = socket (AF_INET ,SOCK_STREAM ,0); //Pedimos un socket enviandole parametros que especifica que utilizamos protocolo TCP/ IP
+
+	    if (bind(socketPedido, (void*) &direccionServidor ,sizeof(direccionServidor)) != 0) //Colomos un bind para que no rompa el codigo.
+	    {                                                                                   //Si falla me devuelve un codigo != de cero.
+	    	perror("Fallo el bind!");
+	    	return 1;
+
+	    }//End if
+
+	    printf("Estoy escuchando\n");
+	    listen(socketPedido, SOMAXCONN); // Le pasamos como parametros el socket y un maximo de conexiones, esas conexiones iran a una cola
+	                                    // las cuales iremos aceptando. (SOMAXCONN es el numero maximo de conexiones)
+
+
 	return EXIT_SUCCESS;
 }
--- Kernel/src/Kernel.h
+++ Kernel/src/Kernel.h
@@ -3,9 +3,13 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<commons/log.h>
 #include<commons/string.h>
 #include<commons/config.h>
 #include<readline/readline.h>
+#include<arpa/inet.h>           //Implemento librerias para utilizar Sockets de flujo.
+#include<sys/socket.h>
+
 
 #endif /* KERNEL_H_ */
