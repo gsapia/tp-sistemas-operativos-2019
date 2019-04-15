83f94c75677542c8655fc53ccdf08d0d2ccecf79
Guillermo Sapia <guillermosapia.lsp@hotmail.com.ar>
2019-04-14 23:48:47 -0300

    update kernel api y librerias

--- Kernel/src/Kernel.h
+++ Kernel/src/Kernel.h
@@ -10,6 +10,21 @@
 #include<readline/readline.h>
 #include<arpa/inet.h>           //Implemento librerias para utilizar Sockets de flujo.
 #include<sys/socket.h>
+#include<unistd.h>
+#include<netdb.h>
+#include <pthread.h>
 
+
+#define SELECT "SELECT"
+#define INSERT "INSERT"
+#define CREATE "CREATE"
+#define DESCRIBE "DESCRIBE"
+#define DROP "DROP"
+#define JOURNAL "JOURNAL"
+#define ADD "ADD"
+#define RUN "RUN"
+#define METRICS "METRICS"
+
+t_log* logger;
 
 #endif /* KERNEL_H_ */
